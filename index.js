9ae534ac3dd61465baaa63fb550278a486125624
Nguyen Kien <ngkien299@gmail.com>
2023-01-06 13:01:53 +0700

    Express Simple REST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'),
   http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 5000;
@@ -8,8 +9,62 @@ const port = 5000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
+
+app.all('/nations', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+app.get('/nations', (req, res, next) => {
+  res.end('Will send all the nations to you!');
+});
+
+app.post('/nations', (req, res, next) => {
+  res.end(
+    'Will add the nation: ' +
+      req.body.name +
+      ' with details: ' +
+      req.body.description
+  );
+});
+
+app.put('/nations', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /nations');
+});
+
+app.delete('/nations', (req, res, next) => {
+  res.end('Deleting all nations');
+});
+
+app.get('/nations/:nationId', (req, res, next) => {
+  res.end(
+    'Will send details of the nation: ' + req.params.nationId + ' to you!'
+  );
+});
+
+app.post('/nations/:nationId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /nations/' + req.params.nationId);
+});
+
+app.put('/nations/:nationId', (req, res, next) => {
+  res.write('Updating the nation: ' + req.params.nationId + '\n');
+  res.end(
+    'Will update the nation: ' +
+      req.body.name +
+      ' with details: ' +
+      req.body.description
+  );
+});
+
+app.delete('/nations/:nationId', (req, res, next) => {
+  res.end('Deleting nation: ' + req.params.nationId);
+});
 
 app.use((req, res, next) => {
   console.log(req.headers);
